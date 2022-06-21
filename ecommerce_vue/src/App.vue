180436ff6e2b3b6d863430becef7a496665e4284
abc <abc@gmail.com>
2022-06-21 17:22:11 +0100

    first commit

---
+++ ecommerce_vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div id="wrapper">
+      <nav class="navbar is-dark">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item"><strong>Ecommerce</strong></router-link>
+          
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div class="navbar-menu" id="navbar-menu"></div>
+      </nav>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
